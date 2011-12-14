eaab7f7713a9dc9b9e18c8f2d9c85881cb79281c
KalimochoAz <calimochoazucarado@gmail.com>
2011-12-14 01:34:51 +0100

    First step to ICS port

---
+++ cm.mk
@@ -0,0 +1,14 @@
+$(call inherit-product, device/samsung/crespo4g/full_crespo4g.mk)
+
+PRODUCT_RELEASE_NAME := NS4G
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=sojus BUILD_ID=GWK74 BUILD_FINGERPRINT=google/sojus/crespo4g:2.3.7/GWK74/185293:user/release-keys PRIVATE_BUILD_DESC="sojus-user 2.3.7 GWK74 185293 release-keys" BUILD_NUMBER=185293
+
+PRODUCT_NAME := cm_crespo4g
+PRODUCT_DEVICE := crespo4g
+
